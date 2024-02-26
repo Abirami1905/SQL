8c942603c7423a3e3c46e3dbeb9a47e7dcefb7dc
Abirami1905 <146990167+Abirami1905@users.noreply.github.com>
2024-02-26 23:14:08 +0530

    Add files via upload

---
+++ select.sql
@@ -0,0 +1,13 @@
+create table science_class(
+stu_id int,
+	name varchar,
+	age int);
+	
+	insert into science_class
+	values(1,'popoye',33),
+	(2,'olive',54),
+	(3,'brutus',98);
+	
+	select name from science_class;
+	
+	

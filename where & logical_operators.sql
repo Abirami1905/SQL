bbc203cb187119c7bfb63e40e4f9c18f1590f600
Abirami1905 <146990167+Abirami1905@users.noreply.github.com>
2024-02-27 23:21:03 +0530

    Add files via upload

---
+++ where & logical_operators.sql
@@ -0,0 +1,5 @@
+select distinct  email_id,age from customer where age > 20 and age<25; 
+
+select distinct  email_id,age from customer where  not age = 21 and email_id='bb@'; 
+
+select * from customer
